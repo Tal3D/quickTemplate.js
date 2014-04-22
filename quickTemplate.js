bd724049046302e7d64c68b6d69f6f1a310c1fa7
Tal3D <taldat3d@gmail.com>
2014-04-22 13:31:59 +0200

    Update.

--- quickTemplate.js
+++ quickTemplate.js
@@ -1,13 +1,21 @@
-function render(){
+/**
+ * Renders the html of the specified target object.
+ * @param   {String}    target    A string which will be rendered.
+ * @param   {Object}    source    The source object whose information will be rendered into the target string.
+ * @return  {String}    Returns the rendered string.
+ */
+function render(target, source){
     var regEx = /\{\S+\}/g;
-    var vars = document.body.innerHTML.match(regEx);
+    var vars = target.match(regEx);
+    var targetString = target;
     for(var i = 0; i < vars.length; i++){
-        var input = eval("info." + vars[i].substr(1, vars[i].length - 2));
+        var input = eval("source." + vars[i].substr(1, vars[i].length - 2));
         if(input){
-            document.body.innerHTML = document.body.innerHTML.replace(vars[i], input);
+            targetString = targetString.replace(vars[i], input);
         }
         else{
-            document.body.innerHTML = document.body.innerHTML.replace(vars[i], "");
+            targetString = targetString.replace(vars[i], "");
         }
     }
+    return targetString;
 }
